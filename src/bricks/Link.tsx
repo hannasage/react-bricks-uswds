51cb242fc3f60eec6c93c3ed6195a0c4d5f05325
Kevin Haube <khaube@fearless.tech>
2022-12-25 02:36:09 -0500

    Adds USLink bricks

--- src/bricks/Link.tsx
+++ src/bricks/Link.tsx
@@ -2,37 +2,106 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { StylesAndChildren } from "../util/genericProps";
 
-enum LinkUse {
+export enum USLinkUse {
+  STD = "standard",
+  EXT = "external",
   NAV = "nav",
   CRUMB = "breadcrumb",
 }
 
-interface LinkProps extends StylesAndChildren {
+interface USLinkProps extends StylesAndChildren {
   href: string;
-  usage?: LinkUse;
-  activeClassName?: string; // ignored if nav is false
+  usage?: USLinkUse;
+  activeClassName?: string;
 }
-export const USLink = ({
+type USNavLinkProps = Omit<USLinkProps, "usage">;
+type USLinkBrickProps = Omit<USLinkProps, "usage" | "activeClassName">;
+
+/** A single link brick to replace NavLink (react-router-dom) */
+export const USNavLink = ({
   href,
   children,
+  className,
+  activeClassName,
+}: USNavLinkProps) => {
+  const navLinkStyles = ["usa-nav__link"];
+  // Extracted functionality for active vs standard classNames
+  const getStyles = (isActive: boolean) => {
+    if (isActive) {
+      navLinkStyles.push(activeClassName || "");
+    } else {
+      navLinkStyles.push(className || "");
+    }
+    return navLinkStyles.join(" ");
+  };
+  return (
+    <NavLink to={href} className={({ isActive }) => getStyles(isActive)}>
+      {children}
+    </NavLink>
+  );
+};
+
+/** A single link brick for rendering external links */
+export const USExtLink = ({ href, className, children }: USLinkBrickProps) => {
+  const linkStyles = ["usa-link", "usa-link--external", className];
+  return (
+    <a href={href} className={linkStyles.join(" ")}>
+      {children}
+    </a>
+  );
+};
+
+/** A single link brick for building breadcrumbs */
+export const USCrumbLink = ({
+  href,
+  className,
+  children,
+}: USLinkBrickProps) => (
+  <a href={href} className={`usa-breadcrumb__link ${className}`}>
+    {children}
+  </a>
+);
+
+/** Because links are fairly small UI components, all of them are pretty much `bricks`. However, to maintain
+ * the brick-like mentality, all subcomponents are broken out into their own bricks that can be called in
+ * JSX. To use them through this single component, you can utilize the `usage` prop and enumerated values to
+ * select your desired brick.
+ */
+export const USLink = ({
+  children,
+  href,
   activeClassName,
   className,
-  usage,
-}: LinkProps) => {
-  if (usage === LinkUse.NAV) {
+  usage = USLinkUse.STD, // default to standard usage
+}: USLinkProps) => {
+  if (usage === USLinkUse.NAV) {
     return (
-      <NavLink
-        to={href}
-        className={(isActive) => (isActive ? activeClassName : className)}
+      <USNavLink
+        href={href}
+        className={className}
+        activeClassName={activeClassName}
       >
         {children}
-      </NavLink>
-    );
-  } else {
-    return (
-      <a href={href} className="usa-link">
-        <span>Home</span>
-      </a>
+      </USNavLink>
     );
   }
+  if (usage === USLinkUse.CRUMB) {
+    return (
+      <USCrumbLink href={href} className={className}>
+        {children}
+      </USCrumbLink>
+    );
+  }
+  if (usage === USLinkUse.EXT) {
+    return (
+      <USExtLink href={href} className={className}>
+        {children}
+      </USExtLink>
+    );
+  }
+  return (
+    <a href={href} className={`usa-link ${className}`}>
+      {children}
+    </a>
+  );
 };
---
+++ src/stories/Link.stories.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { ComponentStory } from "@storybook/react";
+import { BrowserRouter } from "react-router-dom";
+import {
+  USCrumbLink,
+  USExtLink,
+  USLink,
+  USLinkUse,
+  USNavLink,
+} from "../bricks/Link";
+
+//TODO: Update docs with examples
+// const Docs = () => {
+//   return <p>Testing, testing!</p>;
+// };
+
+export default {
+  title: "Links",
+  component: USLink,
+  subcomponents: { USExtLink, USCrumbLink, USNavLink },
+  // parameters: {
+  //   docs: {
+  //     page: () => <Docs />,
+  //   },
+  // },
+};
+
+export const Standard = () => (
+  <USLink href="/?path=/story/link--standard">This is a standard link</USLink>
+);
+
+export const ExternalBrick = () => (
+  <USExtLink href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+    Click for good music
+  </USExtLink>
+);
+
+export const BreadcrumbBrick = () => (
+  <USCrumbLink href="/?path=/story/link--standard">A Breadcrumb</USCrumbLink>
+);
+
+export const NavLinkBrick = () => (
+  <BrowserRouter>
+    <USNavLink href="/?path=/story/link--standard">
+      A NavLink Replacement
+    </USNavLink>
+  </BrowserRouter>
+);
